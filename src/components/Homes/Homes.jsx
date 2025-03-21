32368230478988b1a55d459293ba3c32cf5c2f69
claudiajay <claudiajoppa@gmail.com>
2025-03-21 09:13:56 +0000

    update

--- src/components/Homes/Homes.jsx
+++ src/components/Homes/Homes.jsx
@@ -1,128 +1,100 @@
-import React from 'react'
-import './Homes.css'
-import kitchen from '../../assets/kitchen.png'
-import outside from '../../assets/outside.png'
-import seating from '../../assets/seating.png'
-import location from '../../assets/location.png'
-import bed from '../../assets/bed.png'
-import bath from '../../assets/bath.png'
-import sqft from '../../assets/sqft.png'
+import React from "react";
+import "./Homes.css";
+import kitchen from "../../assets/kitchen.png";
+import outside from "../../assets/outside.png";
+import seating from "../../assets/seating.png";
+import location from "../../assets/location.png";
+import bed from "../../assets/bed.png";
+import bath from "../../assets/bath.png";
+import sqft from "../../assets/sqft.png";
+
+const homesData = [
+  {
+    id: 1,
+    image: kitchen,
+    title: "Skyper Pool Apartment",
+    address: "1020 Bloomingdale Ave",
+    price: "$280,000",
+    badge: "FOR SALE",
+    beds: 4,
+    baths: 2,
+    sgft: 450,
+  },
+  {
+    id: 2,
+    image: outside,
+    title: "North Dillard Street",
+    address: "4330 Bell Shoals Rd",
+    price: "$250",
+    priceSuffix: "/month",
+    badge: "FOR RENT",
+    beds: 4,
+    baths: 2,
+    sgft: 450,
+  },
+  {
+    id: 3,
+    image: seating,
+    title: "Eaton Garth Penthouse",
+    address: "7722 18th Ave, Brooklyn",
+    price: "$180,000",
+    badge: "FOR SALE",
+    featured: true,
+    beds: 4,
+    baths: 2,
+    sgft: 450,
+  },
+];
 
 const Homes = () => {
   return (
-    <div className='homes container'>
+    <div className="homes container">
       <div className="home">
         <h2>Homes For You</h2>
         <p>Based on your view history</p>
-
         <div className="cards">
-
-        <div className='card'>
-        <img src={kitchen} className='card-img'/>
-          <div className='description'>
-          <div className='property'>
-            <h4 >Skyper Pool Apartment</h4>
-            <div className='location'>
-              <img src={location} className='vector'/>
-              <p>1020 Bloomingdale Ave</p>
+          {homesData.map((home) => (
+            <div className="card" key={home.id}>
+              <div className="badges">
+                <span className="badge">{home.badge}</span>
+                {home.featured && (
+                  <span className="badge featured">FEATURED</span>
+                )}
+              </div>
+              <img src={home.image} className="card-img" alt={home.title} />
+              <div className="description">
+                <div className="property">
+                  <h4>{home.title}</h4>
+                  <div className="location">
+                    <img src={location} className="vector" alt="location" />
+                    <p>{home.address}</p>
+                  </div>
+                </div> 
+                <div className={`price ${home?.badge === "FOR RENT" ? "rent" : "sale"}`}>
+                  {home?.price ?? "N/A"}{" "}
+                  {home?.priceSuffix && <span>{home.priceSuffix}</span>}
+                </div>
+              </div>
+              <div className="details">
+                <div className="attribute">
+                  <img src={bed} className="vector" alt="bed" />
+                  <p>{home.beds} Beds</p>
+                </div>
+                <div className="attribute">
+                  <img src={bath} className="vector" alt="bath" />
+                  <p>{home.baths} Baths</p>
+                </div>
+                <div className="attribute">
+                  <img src={sqft} className="vector" alt="sqft" />
+                  <p>{home.sqft} sqft</p>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="price">
-            $280,000
-          </div>
-
-          </div>
-
-          <div className='details'>
-            <div className='attribute'>
-              <img src={bed} className='vector'/>
-              <p>4 Beds</p>
-            </div>
-            <div className='attribute'>
-              <img src={bath} className='vector'/>
-              <p>2 Baths</p>
-            </div>
-            <div className='attribute'>
-              <img src={sqft} className='vector'/>
-              <p>450 sqft</p>
-            </div>
-          </div>  
+          ))}
         </div>
-
-
-        <div className='card'>
-        <img src={outside} className='card-img'/>
-          <div className='description'>
-          <div className='property'>
-            <h4>North Dillard Street</h4>
-            <div className='location'>
-              <img src={location} className='vector'/>
-              <p>4330 Bell Shoals Rd</p>
-            </div>
-          </div>
-          <div className="price">
-            $250<span>/month</span>
-          </div>
-
-          </div>
-
-          <div className='details'>
-            <div className='attribute'>
-              <img src={bed} className='vector'/>
-              <p>4 Beds</p>
-            </div>
-            <div className='attribute'>
-              <img src={bath} className='vector'/>
-              <p>2 Baths</p>
-            </div>
-            <div className='attribute'>
-              <img src={sqft} className='vector'/>
-              <p>400 sqft</p>
-            </div>
-          </div>  
-        </div>
-
-
-        <div className='card'>
-        <img src={seating} className='card-img'/>
-          <div className='description'>
-          <div className='property'>
-            <h4>Eaton Garth Penthouse</h4>
-            <div className='location'>
-              <img src={location} className='vector'/>
-              <p>7722 18th Ave, Brooklyn</p>
-            </div>
-          </div>
-          <div className="price">
-            $180,000
-          </div>
-
-          </div>
-
-          <div className='details'>
-            <div className='attribute'>
-              <img src={bed} className='vector'/>
-              <p>4 Beds</p>
-            </div>
-            <div className='attribute'>
-              <img src={bath} className='vector'/>
-              <p>2 Baths</p>
-            </div>
-            <div className='attribute'>
-              <img src={sqft} className='vector'/>
-              <p>450 sqft</p>
-            </div>
-          </div>  
-        </div>
-        
-
-
-        </div>
-        
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Homes
+export default Homes;
--- src/components/Property/Property.jsx
+++ src/components/Property/Property.jsx
@@ -1,30 +1,58 @@
-import React from 'react'
-import './Property.css'
-import link1 from '../../assets/Link.png'
-import link2 from '../../assets/Link-2.png'
-import link3 from '../../assets/Link-3.png'
-import link4 from '../../assets/Link-4.png'
-import link5 from '../../assets/Link-5.png'
-import link6 from '../../assets/Link-6.png'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-cards'
+import 'swiper/css/pagination';
+import { Pagination, Scrollbar } from 'swiper/modules';
+import chicago from '../../assets/chicago.png';
+import losAngeles from '../../assets/losangeles.png';
+import miami from '../../assets/miami.png';
+import flordia from '../../assets/florida.png';
+import newYork from '../../assets/nyc.png';
+import './Property.css';
 
+
+const properties = [
+  {city: 'Chicago', count:2, img: chicago },
+  {city: 'Los Angeles', count:1, img: losAngeles},
+  {city: 'Miami', count: 2, img: miami },
+  {city: 'Flordia', count: 3, img: flordia},
+  {city: 'New York', count: 8, img: newYork}
+]
 const Property = () => {
   return (
-    <div className='property container'>
-      <div className='props'>
-        <h2>Find Properties in These Cities</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <div className='image-grid'>
-          <img src={link1} className='item'/>
-          <img src={link2} className='item'/>
-          <img src={link3} className='item'/>
-          <img src={link4} className='item'/>
-          <img src={link5} className='item'/>
-          <img src={link6} className='item'/>
-        </div>
-      </div>
-      
+    <div className="property container">
+      <h2 className='title'>Find Properties in These Cities</h2>
+      <p className='subtitle'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <Swiper
+      slidesPerview={4}
+      spaceBetween={6}
+      pagination={{clickable: true}}
+      modules={[Pagination]}
+      className="swiper"
+      breakpoints={{
+        640: {slidesPerview: 1},
+        768: {slidesPerview: 2},
+        1024: {slidesPerview: 3} 
+      }}
+      >
+       {properties.map((props, index) => (
+        <SwiperSlide key={index} >
+          <div className="slide">
+            <img src={props.img} alt={props.city} className='city-image'/>
+            <div className="overlay">
+              <h3>{props.city}</h3>
+              <p>{props.count} Properties</p>
+            </div>
+
+          </div>
+
+        </SwiperSlide>
+       ))}
+
+      </Swiper>
     </div>
   )
 }
 
-export default Property
+export default Property;
